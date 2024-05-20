d8b6497e89ca84747ccd44c51ab79c1d3e412926
Sharma <vijay-sai.sharma@capgemini.com>
2024-05-20 23:42:19 +0530

    Added myinfo controller

---
+++ module5-solution/src/public/myinfo/myinfo.controller.js
@@ -0,0 +1,15 @@
+(function(){
+    angular.module('public')
+        .controller('MyInfoController', MyInfoController);
+    
+    MyInfoController.$inject = ['$scope', '$http', 'MenuService'];
+
+    function MyInfoController($scope, $http, MenuService){
+        $scope.myinfoPrefs = MenuService.getSignupPrefs();
+        if($scope.myinfoPrefs === null){
+            $scope.showprefs = false;
+        }else{
+            $scope.showprefs = true;
+        };
+    }
+})();
