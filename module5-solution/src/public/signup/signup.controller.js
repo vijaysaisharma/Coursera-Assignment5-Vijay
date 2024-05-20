632f0a385d9ef261c5ac83748cd110d5b07672a9
Sharma <vijay-sai.sharma@capgemini.com>
2024-05-20 22:21:07 +0530

    Initial commit for signup functionality

---
+++ module5-solution/src/public/signup/signup.controller.js
@@ -0,0 +1,60 @@
+(function(){
+    'use strict';
+
+    angular.module('public')
+        .controller('SignupController', SignupController)
+        .constant('APIBasePath', 'https://coursera-jhu-default-rtdb.firebaseio.com/');
+    
+        SignupController.$inject = ['$scope', '$http', 'MenuService'];
+
+    function SignupController($scope, $http, MenuService){
+        $scope.user = {};
+        $scope.errorMessage = '';
+        $scope.submitForm = function(isValid){
+            console.log('in submit form in controller');
+
+            if(isValid){
+                const input = $scope.user.menuShortNameFav;
+                const match = input.match(/^([a-zA-Z]+)(\d)$/);
+                var short_name;
+                var menu_num;
+                if(match){
+                    short_name = match[1];
+                    menu_num = match[2];
+                } else{
+                    throw new Error("The input string does not match the expected format.");
+                };
+                $scope.errorMessage = 'Valid Message: ' + short_name + " # " + menu_num;
+                var url = APIBasePath + "/menu_items/" + short_name + "/" + input + ".jpg";
+                $scope.fav_img_url = "https://" + window.location.host + "/images/menu/" + short_name + input + ".jpg";
+                $scope.menu_num = menu_num;
+                $http.get(url)
+                    .then(function(response){
+                        if(response.status === 200 && response.data){
+                            const preferences = {
+                                firstName: $scope.user.firstName,
+                                lastName: $scope.user.lastName,
+                                email: $scope.user.email,
+                                phone: $scope.user.phone,
+                                fav_title: response.data["name"],
+                                fav_description: response.data["description"],
+                                fav_img_url: $scope.fav_img_url
+                            };
+                            MenuService.saveSignupPref(preferences);
+                            $scope.responseData = "Your information has been saved.";
+                            $scope.errorMessage = '';
+                        } else{
+                            $scope.errorMessage = 'No such menu item exists.';
+                            $scope.responseData = '';
+                        };
+                    })
+                    .catch(function(error){
+                        $scope.errorMessage = 'No such menu item exists.';
+                        $scope.responseData = '';
+                    });
+            } else {
+                $scope.errorMessage = 'Please fill the form correctly.';
+            };
+        };
+    }
+})()
